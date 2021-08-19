0c963347ffec522c7e4ed7e99b138f090c8a7aad
MastewalB <se.mastewal.belete@gmail.com>
2021-08-19 23:30:31 +0300

    Animation Complete

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,118 +1,129 @@
 const instructions_form = document.querySelector('form.add-instruction');
 const data_form = document.querySelector('form.add-data');
 const instructions_ul = document.querySelector('.instruction-form .divTable')
+
+const pc_cell = document.querySelector('.cpu-register .pc')
+const ac_cell = document.querySelector('.cpu-register .ac')
+const ir_cell = document.querySelector('.cpu-register .ir')
+
 const data_ul = document.querySelector('.data-form .divTable')
-// const compute = document.querySelector('button.btn-compute');
+const playButton = document.querySelector('.play');
+const pauseButton = document.querySelector('.pause');
+
 const alertDiv = document.querySelector('.alerts');
 const cpuRegisters = document.querySelector('.cpu-registers-container');
 const inputsContainer = document.querySelector('.user-inputs-container');
 
-console.log(instructions_form, instructions_ul)
+//console.log(instructions_form, instructions_ul)
+let process_count_begin = 300;
+let data_count_begin = 940;
 
-let instruction_index = 301;
-let data_index = 940
+let PC = 300;
+let data_index = 940;
 
-instruction_set = new Map() 
-data_set = new Map()
+instruction_set = new Map();
+data_set = new Map();
 
 // Adding a new instruction to instructions list 
 instructions_form.addEventListener('submit', e => {
-    e.preventDefault();
-    
-    const instruction = instructions_form.addnew.value.trim();
-    if (instruction != "") {
-        // const text_span = createNewElement("span", " ", instruction)
-        const newRow = createNewElement(instruction_index, " ", instruction)
-        
-        instructions_ul.innerHTML += newRow;
-        instruction_set[instruction_index] = instruction;
-        instruction_index ++;
-        instructions_form.reset();
-    }
+  e.preventDefault();
+
+  const instruction = instructions_form.addnew.value.trim();
+  if (instruction != "") {
+    // const text_span = createNewElement("span", " ", instruction)
+    const newRow = createNewElement(PC, " ", instruction)
+
+    instructions_ul.innerHTML += newRow;
+    instruction_set.set(PC, instruction);
+    PC++;
+    console.log(instruction_set);
+    instructions_form.reset();
+  }
 });
 
 // Adding new inputs to inputs list 
-data_form.addEventListener('submit', e =>{
-    e.preventDefault();
-    const data = data_form.addData.value.trim();
+data_form.addEventListener('submit', e => {
+  e.preventDefault();
+  const data = data_form.addData.value.trim();
 
-    if ( data != ""){
-        // const text_span = createNewElement("span", " ", )
-        const newDataRow = createNewElement(data_index, " ", fix_zeros(data))
-        data_ul.innerHTML += newDataRow;
-        data_set[data_index] = data;
-        data_index ++;
-        data_form.reset();
-    }
+  if (data != "") {
+    // const text_span = createNewElement("span", " ", )
+    const newDataRow = createNewElement(data_index, " ", fix_zeros(data))
+    data_ul.innerHTML += newDataRow;
+    data_set.set(data_index, data);
+    data_index++;
+    console.log(data_set);
+    data_form.reset();
+  }
 });
 
 // add prefix zeros to input numbers 
 const fix_zeros = num => {
-    while (num.length < 4) {
-        num = "0" + num
-    }
-    return num
+  while (num.length < 4) {
+    num = "0" + num
+  }
+  return num;
 }
 
 //  create new HTML elements ----------- for UI trees 
 const createNewElement = (index, class_name = " ", data) => {
-    const html = `
+  const html = `
         <div class="divRow">
             <div class="divCellNums">${index}</div>
-            <div class="divCell">${data}</div>
+            <div class="divCell _${PC}">${data}</div>
         </div>
     `
-    if (class_name != " ") {
-        newElement.setAttribute('class', class_name);
-    }
-    return html
+  if (class_name != " ") {
+    newElement.setAttribute('class', class_name);
+  }
+  return html
 }
 
 
 // alert box close 
-alertDiv.querySelector("button").addEventListener('click', e => {
-    document.querySelector('.alerts').classList.add('invisible')
-})
+// alertDiv.querySelector("button").addEventListener('click', e => {
+//   document.querySelector('.alerts').classList.add('invisible')
+// })
 
 // draw focus on a box  
-const drawFocus = (component, duration) =>{
-        component.classList.add('flashy-border')
-        console.log("animate box", component)
-        setTimeout(()=>{
+const drawFocus = (component, duration) => {
+  component.classList.add('flashy-border')
+  //console.log("animate box", component)
+  setTimeout(() => {
 
-            component.classList.remove('flashy-border')
-        }, duration)
-        // const interval= setInterval(()=>{
-        //     timesRun += 1;
-        //     if(timesRun === 6){
-        //         clearInterval(interval);
-        //     }
-        // }, 600)
+    component.classList.remove('flashy-border')
+  }, duration)
+  // const interval= setInterval(()=>{
+  //     timesRun += 1;
+  //     if(timesRun === 6){
+  //         clearInterval(interval);
+  //     }
+  // }, 600)
 }
 
 
 // start button 
 // get rid of input fields and activate animation tab 
 
-// compute.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     console.log(instruction_set)
-//     console.log(data_set)
+playButton.addEventListener('click', (e) => {
+  e.preventDefault();
 
-//     if (instructions_ul.childElementCount == 0) {
-//         if (alertDiv.classList.contains('invisible')) {
-//             document.querySelector('.alerts').classList.remove('invisible')
-//         }
-//     } else {
-//         inputsContainer.classList.add('invisible')
-//         cpuRegisters.classList.remove('invisible')
-//     }
-// })
+  if (instructions_ul.childElementCount == 0) {
+    if (alertDiv.classList.contains('invisible')) {
+      //document.querySelector('.alerts').classList.remove('invisible')
+    }
+  } else {
+    execute_instruction(process_count_begin);
+  }
+});
 
 
 
+pauseButton.addEventListener('click', (e) => {
+  e.preventDefault();
 
 
+});
 
 
 
@@ -140,7 +151,6 @@ let memory = new Map([
 ]);
 
 
-let PC = 300; //Process Counter
 let AC = ''; //Accumulator
 let IR = ''; //Instruction Register
 
@@ -152,11 +162,44 @@ let IR = ''; //Instruction Register
 * @return returns the instruction on the address 
 */
 function fetch_instruction(process_address) {
-  if (memory.get(process_address) != null) {
-      IR = memory.get(process_address);
-      return true;
+  if (instruction_set.get(process_address) != null) {
+
+    //change
+    IR = instruction_set.get(process_address);
+    console.log(IR);
+    var pc_str = '._' + (process_count_begin).toString();
+    var element = document.querySelector(pc_str);
+
+    process_count_begin++;
+
+    anime({
+      targets: element,
+      translateX: [
+        { value: 404, duration: 900 },
+      ],
+      translateY: [
+        { value: 80, duration: 500 }
+      ],
+      rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+      },
+      update: function () {
+        pc_cell.innerText = process_count_begin;
+
+      }
+    });
+
+    return true;
   }
   return null;
+}
+
+function instruction_available(PC) {
+  if (instruction_set.get(PC) != null) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -167,37 +210,36 @@ function interpret_instruction(instruction) {
 }
 
 
-function execute_instruction(PC) {
+function execute_instruction(process_count_begin) {
 
-  while (fetch_instruction(PC) != null) {
+  while (instruction_available(process_count_begin) == true) {
 
-      fetch_instruction(PC);
+    fetch_instruction(process_count_begin);
+    let { opcode, data } = interpret_instruction(IR);
 
-      let { opcode, data } = interpret_instruction(IR);
 
-
-      PC++;
-      if (opcode === 1) {
-          AC = load(data, memory);
-      }
-      else if (opcode === 2) {
-          store(data, AC, memory);
-      }
-      else if (opcode === 5) {
-          AC = add(AC, load(data, memory));
-      }
-      console.log({
-          "PC": PC,
-          "AC": AC,
-          "data": data,
-      });
+    process_count_begin++;
+    if (opcode === 1) {
+      AC = load(data, data_set);
+    }
+    else if (opcode === 2) {
+      store(data, AC, data_set);
+    }
+    else if (opcode === 5) {
+      AC = add(AC, load(data, data_set));
+    }
+    console.log({
+      "PC": process_count_begin,
+      "AC": AC,
+      "data": data,
+    });
   }
-  console.log(memory);
+  //console.log(memory);
 }
 
 
 
-console.log(execute_instruction(300));
+//console.log(execute_instruction(300));
 
 
 function add(bin1, bin2) {
@@ -208,27 +250,26 @@ function subtract(bin1, bin2) {
   return bin1 - bin2;
 }
 
-function load(address, memory) {
-  return memory.get(address) * 1;
+function load(address, data_set) {
+  return data_set.get(address) * 1;
 }
 
-function store(address, value, memory) {
-  return memory.set(address, (value).toString());
+function store(address, value, data_set) {
+  return data_set.set(address, (value).toString());
 }
 
-// End of code from Maste
 
-anime({
-  targets: '#smt',
-  translateY: [
-    { value: 200, duration: 500 },
-    { value: 0, duration: 800 }
+
+
+var animate = anime({
+  targets: `.${PC}`,
+  translateX: [
+    { value: 200, duration: 900 },
+    { value: 0, duration: 900 }
   ],
-  rotate:{
+  rotate: {
     value: '1turn',
     easing: 'easeInOutSine'
-  }
+  },
+  autoplay: false
 });
-console.log("I was here!");
-Arrow.show(5000);
-console.log("I was here to!");
--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const inputsContainer = document.querySelector('.user-inputs-container');
 let instruction_index = 300;
 let data_index = 940
 
-instruction_set = new Map() 
+instruction_set = new Map()
 data_set = new Map()
 
 // Adding a new instruction to instructions list 
 instructions_form.addEventListener('submit', e => {
     e.preventDefault();
-    
+
     const instruction = instructions_form.addnew.value.trim();
     if (instruction != "") {
         const text_span = createNewElement("span", " ", instruction)
@@ -25,23 +25,25 @@ instructions_form.addEventListener('submit', e => {
         li.appendChild(text_span);
         instructions_ul.append(li);
         instruction_set[instruction_index] = instruction;
-        instruction_index ++;
+        instruction_index++;
+        console.log(instruction_set);
         instructions_form.reset();
     }
 });
 
 // Adding new inputs to inputs list 
-data_form.addEventListener('submit', e =>{
+data_form.addEventListener('submit', e => {
     e.preventDefault();
     const data = data_form.addNumber.value.trim();
 
-    if ( data != ""){
+    if (data != "") {
         const text_span = createNewElement("span", " ", fix_zeros(data))
         const li = createNewElement("li", "user-input", " ")
         li.appendChild(text_span);
         inputs_ul.append(li);
         data_set[data_index] = data;
-        data_index ++;
+        data_index++;
+        console.log(data_set);
         data_form.reset();
     }
 });
@@ -87,17 +89,17 @@ alertDiv.querySelector("button").addEventListener('click', e => {
 })
 
 // draw focus on a box  
-const drawFocus = (component, duration) =>{
-        component.classList.add('flashy-border')
-        console.log("animate box", component)
-        setTimeout(()=>{
+const drawFocus = (component, duration) => {
+    component.classList.add('flashy-border')
+    console.log("animate box", component)
+    setTimeout(() => {
 
-            component.classList.remove('flashy-border')
-        }, duration)
-        // const interval= setInterval(()=>{
-        //     timesRun += 1;
-        //     if(timesRun === 6){
-        //         clearInterval(interval);
-        //     }
-        // }, 600)
+        component.classList.remove('flashy-border')
+    }, duration)
+    // const interval= setInterval(()=>{
+    //     timesRun += 1;
+    //     if(timesRun === 6){
+    //         clearInterval(interval);
+    //     }
+    // }, 600)
 }
